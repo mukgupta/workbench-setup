e5bab5c0a6151d4b7141ffb363467d12053a5938
Mukesh Gupta <mukeshgupta.2006@gmail.com>
2013-08-16 02:21:07 +0530

    First Commit

---
+++ setup.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+# Simple setup.sh for configuring Ubuntu 12.04 LTS EC2 instance
+# for headless setup. 
+
+# Install nvm: node-version manager
+# https://github.com/creationix/nvm
+sudo apt-get install -y git-core
+sudo apt-get install build-essential
+sudo apt-get install libmysqlclient-dev
+$sudo apt-get install python-dev
+
+
+
